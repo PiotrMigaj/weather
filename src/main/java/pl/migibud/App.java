91b2854d2839a688f988b32bccd6cfe92f84efa5
Michal Paukszto <michal.paukszto@gmail.com>
2022-07-17 12:52:20 +0200

    michal's commit

--- src/main/java/pl/migibud/App.java
+++ src/main/java/pl/migibud/App.java
@@ -3,5 +3,6 @@ package pl.migibud;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Michal tu był");
     }
 }
